f212a5e4860f38a08a6e4fbbd5808db6f09a5042
winghoing <52132805+winghoing@users.noreply.github.com>
2021-08-05 15:13:47 +0800

    Update CrestronConnection.ts

--- src/CrestronConnection.ts
+++ src/CrestronConnection.ts
@@ -11,31 +11,31 @@ export class CrestronConnection {
         this.platform = platform;
         this.creskitConn = new Socket();
         this.port = port;
-	this.host = host;
-        if(this.host != undefined || this.host != ""){
-           this.creskitConn.on("error", this.connErrorEvent.bind(this));
-           this.creskitConn.on("timeout", this.connTimeOutEvent);
-           this.creskitConn.on("connect", this.connectedEvent.bind(this));
-           this.creskitConn.on("data", this.dataEvent.bind(this));
-           this.creskitConn.on("end", this.disconnectedEvent.bind(this));
-           this.connectToHost();
+        this.host = host;
+        if (this.host != undefined || this.host != "") {
+            this.creskitConn.on("error", this.connErrorEvent.bind(this));
+            this.creskitConn.on("timeout", this.connTimeOutEvent);
+            this.creskitConn.on("connect", this.connectedEvent.bind(this));
+            this.creskitConn.on("data", this.dataEvent.bind(this));
+            this.creskitConn.on("end", this.disconnectedEvent.bind(this));
+            this.connectToHost();
         }
     }
 
     connectToHost() {
-        if(this.creskitConn != undefined){
-           console.log(`connect to ${this.host}:${this.port}`);
-	   this.creskitConn.connect(this.port, this.host);
+        if (this.creskitConn != undefined) {
+            console.log(`connect to ${this.host}:${this.port}`);
+            this.creskitConn.connect(this.port, this.host);
         }
     }
 
     connErrorEvent() {
-        if(this.creskitConn != undefined){
-           console.log("connection error");
-           console.log(`reconnect to ${this.host}:${this.port} again`);
-           setTimeout(function (this: CrestronConnection) {
-               this.creskitConn.connect(this.port, this.host);
-           }.bind(this), 2000);
+        if (this.creskitConn != undefined) {
+            console.log("connection error");
+            console.log(`reconnect to ${this.host}:${this.port} again`);
+            setTimeout(function (this: CrestronConnection) {
+                this.creskitConn.connect(this.port, this.host);
+            }.bind(this), 2000);
         }
     }
 
@@ -44,8 +44,8 @@ export class CrestronConnection {
     }
 
     dataEvent(data: string): void {
-        if(this.platform != undefined){
-           this.platform.processData(data.toString());
+        if (this.platform != undefined) {
+            this.platform.processData(data.toString());
         }
     }
 
@@ -54,18 +54,18 @@ export class CrestronConnection {
     }
 
     disconnectedEvent() {
-        if(this.creskitConn != undefined){
-           console.log("disconnected from the host");
-           console.log(`reconnect to ${this.host}:${this.port} again`);
-           setTimeout(function (this: CrestronConnection) {
-               this.creskitConn.connect(this.port, this.host);
-           }.bind(this), 2000);
+        if (this.creskitConn != undefined) {
+            console.log("disconnected from the host");
+            console.log(`reconnect to ${this.host}:${this.port} again`);
+            setTimeout(function (this: CrestronConnection) {
+                this.creskitConn.connect(this.port, this.host);
+            }.bind(this), 2000);
         }
     }
 
     writeData(data: string) {
-        if(this.creskitConn != undefined){
-           this.creskitConn.write(data);
+        if (this.creskitConn != undefined) {
+            this.creskitConn.write(data);
         }
     }
 }
