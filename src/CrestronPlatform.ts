9ce6650121de3fe2305c827f3eca995a35974989
winghoing <52132805+winghoing@users.noreply.github.com>
2021-08-14 14:12:47 +0800

    Update CrestronPlatform.ts

--- src/CrestronPlatform.ts
+++ src/CrestronPlatform.ts
@@ -82,98 +82,100 @@ export class CrestronPlatform implements DynamicPlatformPlugin {
         let configDevices = this.config["accessories"];
         //this.log.info(`printing accessories: ${configDevices}`);
         // loop over the discovered devices and register each one if it has not already been registered
-        for (let device of configDevices) {
+        if(configDevices != undefined) {
+            for (let device of configDevices) {
 
-            // generate a unique id for the accessory this should be generated from
-            // something globally unique, but constant, for example, the device serial
-            // number or MAC address
-            const uuid = this.api.hap.uuid.generate(device.name.toString() + device.id.toString());
+                // generate a unique id for the accessory this should be generated from
+                // something globally unique, but constant, for example, the device serial
+                // number or MAC address
+                const uuid = this.api.hap.uuid.generate(device.name.toString() + device.id.toString());
 
-            // see if an accessory with the same uuid has already been registered and restored from
-            // the cached devices we stored in the `configureAccessory` method above
-            const existingAccessory = this.accessories.find(accessory => accessory.UUID === uuid);
+                // see if an accessory with the same uuid has already been registered and restored from
+                // the cached devices we stored in the `configureAccessory` method above
+                const existingAccessory = this.accessories.find(accessory => accessory.UUID === uuid);
 
-            if (existingAccessory) {
-                // the accessory already exists
-                this.log.info(`Restoring existing accessory from cache: ${existingAccessory.displayName}`);
+                if (existingAccessory) {
+                    // the accessory already exists
+                    this.log.info(`Restoring existing accessory from cache: ${existingAccessory.displayName}`);
 
-                // if you need to update the accessory.context then you should run `api.updatePlatformAccessories`. eg.:
-                existingAccessory.context.device = device;
-                existingAccessory.displayName = device.name;
-                this.api.updatePlatformAccessories([existingAccessory]);
+                    // if you need to update the accessory.context then you should run `api.updatePlatformAccessories`. eg.:
+                    existingAccessory.context.device = device;
+                    existingAccessory.displayName = device.name;
+                    this.api.updatePlatformAccessories([existingAccessory]);
 
-                // create the accessory handler for the restored accessory
-                // this is imported from `platformAccessory.ts`
-                this.log.info(`existing accessory type: ${device.type}`);
-                switch (device.type) {
-                    case "Lightbulb":
-                        {
-                            this.log.info(`create existing lightbulb accessory: ${existingAccessory.displayName}`);
-                            new Lightbulb(this, existingAccessory, this.eventEmitter);
-                            break;
-                        }
-                    case "DimLightbulb":
-                        {
-                            this.log.info(`create existing dimlightbulb accessory: ${existingAccessory.displayName}`);
-                            new DimLightbulb(this, existingAccessory, this.eventEmitter);
-                            break;
-                        }
-                    case "HeaterCooler":
-                        {
-                            this.log.info(`device.minTemperatureValue: ${device.minTemperatureValue}`);
-                            this.log.info(`device.maxTemperatureValue: ${device.maxTemperatureValue}`);
-                            this.log.info(`device.minTemperatureStep: ${device.minTemperatureStep}`);
-                            this.log.info(`device.TemperatureDisplayUnit: ${device.TemperatureDisplayUnit}`);
-                            this.log.info(`create existing heatercooler accessory: ${existingAccessory.displayName}`);
-                            new HeaterCooler(this, existingAccessory, this.eventEmitter);
-                            break;
-                        }
+                    // create the accessory handler for the restored accessory
+                    // this is imported from `platformAccessory.ts`
+                    this.log.info(`existing accessory type: ${device.type}`);
+                    switch (device.type) {
+                        case "Lightbulb":
+                            {
+                                this.log.info(`create existing lightbulb accessory: ${existingAccessory.displayName}`);
+                                new Lightbulb(this, existingAccessory, this.eventEmitter);
+                                break;
+                            }
+                        case "DimLightbulb":
+                            {
+                                this.log.info(`create existing dimlightbulb accessory: ${existingAccessory.displayName}`);
+                                new DimLightbulb(this, existingAccessory, this.eventEmitter);
+                                break;
+                            }
+                        case "HeaterCooler":
+                            {
+                                this.log.info(`device.minTemperatureValue: ${device.minTemperatureValue}`);
+                                this.log.info(`device.maxTemperatureValue: ${device.maxTemperatureValue}`);
+                                this.log.info(`device.minTemperatureStep: ${device.minTemperatureStep}`);
+                                this.log.info(`device.TemperatureDisplayUnit: ${device.TemperatureDisplayUnit}`);
+                                this.log.info(`create existing heatercooler accessory: ${existingAccessory.displayName}`);
+                                new HeaterCooler(this, existingAccessory, this.eventEmitter);
+                                break;
+                            }
+                    }
+
+                    // it is possible to remove platform accessories at any time using `api.unregisterPlatformAccessories`, eg.:
+                    // remove platform accessories when no longer present
+                    // this.api.unregisterPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [existingAccessory]);
+                    // this.log.info('Removing existing accessory from cache:', existingAccessory.displayName);
+                } else {
+                    // the accessory does not yet exist, so we need to create it
+                    this.log.info(`adding new accessory: ${device.name}`);
+
+                    // create a new accessory
+                    const accessory = new this.api.platformAccessory(device.name, uuid);
+
+                    // store a copy of the device object in the `accessory.context`
+                    // the `context` property can be used to store any data about the accessory you may need
+                    accessory.context.device = device;
+
+                    // create the accessory handler for the newly create accessory
+                    // this is imported from `platformAccessory.ts`
+                    switch (device.type) {
+                        case "Lightbulb":
+                            {
+                                this.log.info(`create not existing lightbulb accessory: ${accessory.displayName}`);
+                                new Lightbulb(this, accessory, this.eventEmitter);
+                                break;
+                            }
+                        case "DimLightbulb":
+                            {
+                                this.log.info(`create not existing dimlightbulb accessory: ${accessory.displayName}`);
+                                new DimLightbulb(this, accessory, this.eventEmitter);
+                                break;
+                            }
+                        case "HeaterCooler":
+                            {
+                                this.log.info(`device.minTemperatureValue: ${device.minTemperatureValue}`);
+                                this.log.info(`device.maxTemperatureValue: ${device.maxTemperatureValue}`);
+                                this.log.info(`device.minTemperatureStep: ${device.minTemperatureStep}`);
+                                this.log.info(`device.TemperatureDisplayUnit: ${device.TemperatureDisplayUnit}`);
+                                this.log.info(`create not existing heatercooler accessory: ${accessory.displayName}`);
+                                new HeaterCooler(this, accessory, this.eventEmitter);
+                                break;
+                            }
+                    }
+
+                    // link the accessory to your platform
+                    this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
                 }
-
-                // it is possible to remove platform accessories at any time using `api.unregisterPlatformAccessories`, eg.:
-                // remove platform accessories when no longer present
-                // this.api.unregisterPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [existingAccessory]);
-                // this.log.info('Removing existing accessory from cache:', existingAccessory.displayName);
-            } else {
-                // the accessory does not yet exist, so we need to create it
-                this.log.info(`adding new accessory: ${device.name}`);
-
-                // create a new accessory
-                const accessory = new this.api.platformAccessory(device.name, uuid);
-
-                // store a copy of the device object in the `accessory.context`
-                // the `context` property can be used to store any data about the accessory you may need
-                accessory.context.device = device;
-
-                // create the accessory handler for the newly create accessory
-                // this is imported from `platformAccessory.ts`
-                switch (device.type) {
-                    case "Lightbulb":
-                        {
-                            this.log.info(`create not existing lightbulb accessory: ${accessory.displayName}`);
-                            new Lightbulb(this, accessory, this.eventEmitter);
-                            break;
-                        }
-                    case "DimLightbulb":
-                        {
-                            this.log.info(`create not existing dimlightbulb accessory: ${accessory.displayName}`);
-                            new DimLightbulb(this, accessory, this.eventEmitter);
-                            break;
-                        }
-                    case "HeaterCooler":
-                        {
-                            this.log.info(`device.minTemperatureValue: ${device.minTemperatureValue}`);
-                            this.log.info(`device.maxTemperatureValue: ${device.maxTemperatureValue}`);
-                            this.log.info(`device.minTemperatureStep: ${device.minTemperatureStep}`);
-                            this.log.info(`device.TemperatureDisplayUnit: ${device.TemperatureDisplayUnit}`);
-                            this.log.info(`create not existing heatercooler accessory: ${accessory.displayName}`);
-                            new HeaterCooler(this, accessory, this.eventEmitter);
-                            break;
-                        }
-                }
-
-                // link the accessory to your platform
-                this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
             }
         }
     }
