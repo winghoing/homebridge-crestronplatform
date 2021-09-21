551cccaff5a9a9dedfb0bfd3037b16bffc6c5cc0
winghoing <52132805+winghoing@users.noreply.github.com>
2021-09-21 15:18:56 +0800

    Update HeaterCooler.ts

--- src/HeaterCooler.ts
+++ src/HeaterCooler.ts
@@ -275,14 +275,18 @@ export class HeaterCooler {
     }
     
     updateThresholdTemperature(value: number, type: string) {
+        this.platform.log.info(`Test point 1`);
         let tmpThresholdTemperature = value;
         if(type == "HeatingThresholdTemperature") {
+            this.platform.log.info(`Test point 2`);
             this.service.updateCharacteristic(this.platform.Characteristic.HeatingThresholdTemperature, tmpThresholdTemperature);    
             this.platform.log.info(`${this.deviceType}:${this.id}: Update Characteristic HeatingThresholdTemperature: -> ${tmpThresholdTemperature}`);
         }else if(type == "CoolingThresholdTemperature") {
+            this.platform.log.info(`Test point 3`);
             this.service.updateCharacteristic(this.platform.Characteristic.CoolingThresholdTemperature, tmpThresholdTemperature);
             this.platform.log.info(`${this.deviceType}:${this.id}: Update Characteristic CoolingThresholdTemperature: -> ${tmpThresholdTemperature}`);
         }
+        this.platform.log.info(`Test point 4`);
     }
     
     async handleTemperatureDisplayUnitsSet(value: CharacteristicValue) {
