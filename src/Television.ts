de8772bcafc47f92c036b94dcb63a98840b9809c
winghoing <52132805+winghoing@users.noreply.github.com>
2022-01-15 10:56:35 +0800

    Update Television.ts

--- src/Television.ts
+++ src/Television.ts
@@ -100,7 +100,7 @@ export class Television {
 
         this.service.addLinkedService(input2Service);
 		
-	const speakerService = this.accessory.addService(this.platform.Service.TelevisionSpeaker);
+	const speakerService = this.accessory.getService("TestSpeaker1") || this.accessory.addService(this.platform.Service.TelevisionSpeaker, "TestSpeaker2", "TestSpk2");
 
 	speakerService
 		.setCharacteristic(this.platform.Characteristic.Active, this.platform.Characteristic.Active.ACTIVE)
@@ -111,7 +111,7 @@ export class Television {
 		.onSet((newValue) => {
 			this.platform.log.info('set VolumeSelector => setNewValue: ' + newValue);
 	});
-	this.service.addLinkedService(speakerService);
+	//this.service.addLinkedService(speakerService);
     }
 
     async handleActiveGet(): Promise<CharacteristicValue> {
