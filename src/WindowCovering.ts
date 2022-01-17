d4fb1146e0dad4f442f6cc7c81bd7a6fb916886d
winghoing <52132805+winghoing@users.noreply.github.com>
2022-01-17 18:15:18 +0800

    Create WindowCovering.ts

---
+++ src/WindowCovering.ts
@@ -0,0 +1,116 @@
+import { Service, PlatformAccessory, CharacteristicValue } from "homebridge";
+
+import { CrestronPlatform } from "./CrestronPlatform";
+
+import { EventEmitter } from "events";
+
+export class WindowCovering {
+    private service: Service;
+    private id: number;
+    private eventEmitter: EventEmitter;
+    private deviceType = "WindowCovering";
+    private eventCurrentPositionMsg = "eventCurrentPosition";
+    private getCurrentPositionMsg = "getCurrentPosition";
+    private setTargetPositionMsg = "setTargetPosition";
+
+    /**
+    * These are just used to create a working example
+    * You should implement your own code to track the state of your accessory
+    */
+    private states = {
+        Name: "",
+        CurrentPosition: 0,
+        TargetPosition: 0,
+        PositionState: 2
+    }
+
+    constructor(
+        private platform: CrestronPlatform,
+        private accessory: PlatformAccessory,
+        eventEmitter: EventEmitter
+    ) {
+        this.id = accessory.context.device.id;
+        this.accessory = accessory;
+        this.eventEmitter = eventEmitter;
+        //this.eventEmitter.on(`${this.deviceType}:${this.id}:${this.getCurrentPositionMsg}`, this.getCurrentPositionMsgEvent.bind(this));
+        //this.eventEmitter.on(`${this.deviceType}:${this.id}:${this.eventCurrentPositionMsg}`, this.setCurrentPositionMsgEvent.bind(this));
+        
+        this.states.Name = this.accessory.context.device.name;
+        // set accessory information
+        this.accessory.getService(this.platform.Service.AccessoryInformation)!
+            .setCharacteristic(this.platform.Characteristic.Manufacturer, 'Default-Manufacturer')
+            .setCharacteristic(this.platform.Characteristic.Model, 'Default-Model')
+            .setCharacteristic(this.platform.Characteristic.SerialNumber, 'Default-Serial');
+
+        // get the Television service if it exists, otherwise create a new Television service
+        // you can create multiple services for each accessory
+
+        this.service = this.accessory.getService(this.platform.Service.WindowCovering) || this.accessory.addService(this.platform.Service.WindowCovering);            
+
+        // set the service name, this is what is displayed as the default name on the Home app
+        // in this example we are using the name we stored in the `accessory.context` in the `discoverDevices` method.
+
+        this.service.setCharacteristic(this.platform.Characteristic.Name, this.accessory.context.device.name);
+
+        // each service must implement at-minimum the "required characteristics" for the given service type
+        // see https://developers.homebridge.io/#/service/Television
+
+        this.service.getCharacteristic(this.platform.Characteristic.CurrentPosition)
+            .onGet(this.handleCurrentPositionGet.bind(this))
+            .onSet(this.handleCurrentPositionSet.bind(this));
+
+        this.service.getCharacteristic(this.platform.Characteristic.PositionState)
+            .onGet(this.handlePositionStateGet.bind(this))
+            .onSet(this.handlePositionStateSet.bind(this));
+
+        this.service.getCharacteristic(this.platform.Characteristic.TargetPosition)
+            .onGet(this.handleTargetPositionGet.bind(this))
+            .onSet(this.handleTargetPositionSet.bind(this));
+    }
+
+    async handleCurrentPositionGet(): Promise<CharacteristicValue> {
+        const currentPosition = this.states.CurrentPosition;
+        this.platform.log.info(`${this.deviceType}:${this.id}: Get Characteristic CurrentPosition From Homekit -> ${currentPosition}`);
+        this.platform.sendData(`${this.deviceType}:${this.id}:${this.getCurrentPositionMsg}:*`);
+        return currentPosition;
+    }
+
+    async handlePositionStateGet(): Promise<CharacteristicValue> {
+        const positionState = this.states.PositionState;
+        this.platform.log.info(`${this.deviceType}:${this.id}: Get Characteristic PositionState From Homekit -> ${positionState}`);
+        this.platform.sendData(`${this.deviceType}:${this.id}:${this.getPositionState}:*`);
+        return positionState;
+    }
+
+    async handleTargetPositionGet(): Promise<CharacteristicValue> {
+        const targetPosition = this.states.TargetPosition;
+        this.platform.log.info(`${this.deviceType}:${this.id}: Get Characteristic TargetPosition From Homekit -> ${targetPosition}`);
+        return targetPosition;
+    }
+
+    async handleCurrentPositionSet(value: CharacteristicValue) {
+        const tmpCurrentPosition = value as number;
+        if (this.states.CurrentPosition != tmpCurrentPosition) {
+            this.states.CurrentPosition = tmpCurrentPosition;
+            //this.platform.sendData(`${this.deviceType}:${this.id}:${this.set}:${this.states.CurrentPosition}:*`);
+            this.platform.log.info(`${this.deviceType}:${this.id}: Set Characteristic CurrentPosition By Homekit -> ${tmpCurrentPosition}`);
+        }
+    }
+
+    async handlePositionStateSet(value: CharacteristicValue) {
+        const tmpPositionState = value as number;
+        if (this.states.PositionState != tmpPositionState) {
+            this.states.PositionState = tmpPositionState;
+            //this.platform.sendData(`${this.deviceType}:${this.id}:${this.setInputStateMsg}:${this.states.PositionState}:*`);
+            this.platform.log.info(`${this.deviceType}:${this.id}: Set Characteristic PositionState By Homekit -> ${tmpPositionState}`);
+        }
+    }
+
+    async handleTargetPositionSet(value: CharacteristicValue){
+        const tmpTargetPosition = value as number;
+        if(this.states.TargetPosition != tmpTargetPosition) {
+            this.states.TargetPosition = tmpTargetPosition;
+            this.platform.log.info(`${this.deviceType}:${this.id}: Set Characteristic TargetPosition By Homekit -> ${tmpTargetPosition}`);
+        }
+    }
+}
