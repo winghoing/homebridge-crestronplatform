551b588816c0eb4c0bc3811613d522df2bc0f488
winghoing <52132805+winghoing@users.noreply.github.com>
2022-01-18 12:23:45 +0800

    Update Speaker.ts

--- src/Speaker.ts
+++ src/Speaker.ts
@@ -28,8 +28,8 @@ export class Speaker {
     }
 
     constructor(
-        private platform: CrestronPlatform,
-        private accessory: PlatformAccessory,
+        platform: CrestronPlatform,
+        accessory: PlatformAccessory,
         eventEmitter: EventEmitter
     ) {
         this.id = accessory.context.device.id;
